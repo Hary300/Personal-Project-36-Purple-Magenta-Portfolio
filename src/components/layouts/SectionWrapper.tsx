55101e0162ce9282e4e4dc3b70f2d25126ad9555
Haryadi Nugraha Putra <h.nugraha.p@gmail.com>
2026-09-02 14:03:06 +0700

    feat: add sparkle icon and enhance StatsSection layout with gap support

--- src/components/layouts/SectionWrapper.tsx
+++ src/components/layouts/SectionWrapper.tsx
@@ -4,12 +4,14 @@ import type { ReactNode } from 'react';
 interface SectionWrapperProps {
   sectionId: 'home' | 'about' | 'skill' | 'projects' | 'faq' | (string & {});
   children: ReactNode;
+  hasGap?: boolean;
   className?: string;
 }
 
 const SectionWrapper = ({
   sectionId,
   children,
+  hasGap = false,
   className,
 }: SectionWrapperProps) => {
   return (
@@ -17,6 +19,7 @@ const SectionWrapper = ({
       id={sectionId}
       className={cn(
         'pt-10 pb-10 px-4 sm:px-10 lg:px-15 xl:px-30 lg:pt-20 lg:pb-20 max-w-360 mx-auto',
+        hasGap && 'flex flex-col gap-6 lg:gap-16',
         className
       )}
     >
--- src/sections/02-statsSection/StatsSection.tsx
+++ src/sections/02-statsSection/StatsSection.tsx
@@ -4,13 +4,36 @@ import { statsSectionData } from '@/data/statsSectionData';
 
 const StatsSection = () => {
   const header = statsSectionData.header;
+  const stats = statsSectionData.stats;
+
   return (
-    <SectionWrapper sectionId='stats'>
+    <SectionWrapper sectionId='stats' hasGap>
       <SectionTitle
         title={header.title}
         subtitle={header.subtitle}
         className='text-left'
       />
+      <div className='flex flex-col gap-4 lg:gap-6 divide-y'>
+        {stats.map((stat) => (
+          <div
+            key={stat.id}
+            className='grid grid-cols-[0.8fr_1fr_auto] items-center pb-4 lg:pb-6 gap-2'
+          >
+            <div className='flex  items-center'>
+              <div className="size-7 lg:size-11 bg-linear-to-br from-[#5E2591] to-[#A93FA8] mask-[url('@/assets/icons/sparkle.svg')] mask-contain mask-no-repeat" />
+              <span className='font-bold text-[clamp(28px,calc(10.43vw-5.38px),40px)] lg:text-[80px]'>
+                {stat.value}
+              </span>
+            </div>
+            <p className='font-medium text-md lg:text-display-md max-w-30 lg:max-w-60'>
+              {stat.label}
+            </p>
+            <div className='max-w-20 lg:max-w-30 rounded-md lg:rounded-2xl overflow-hidden'>
+              <img src={stat.image} alt={stat.imageAlt} />
+            </div>
+          </div>
+        ))}
+      </div>
     </SectionWrapper>
   );
 };
